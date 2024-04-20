6a808ddb3d2d12e7b26dd3cfae6309e439f3daa9
Yashraj_K <yashrajkamble7727@gamil.com>
2024-04-20 19:00:37 +0530

    done adding more folder and song on their

--- js/script.js
+++ js/script.js
@@ -80,7 +80,12 @@ async function displayAlbums() {
     // console.log(div)
     let anchors = div.getElementsByTagName("a")
     // console.log(anchors)
-    Array.from(anchors).forEach(async e => {
+    let cardContainer = document.querySelector(".cardContainer")
+    let array = Array.from(anchors)
+    for (let index = 0; index < array.length; index++) {
+        const e = array[index]
+    
+    // Array.from(anchors).forEach(async e => {
         // console.log(e.href)
         if (e.href.includes("/songs/")) {
             // console.log(e.href)
@@ -90,9 +95,37 @@ async function displayAlbums() {
             let a = await fetch(`http://127.0.0.1:5500/songs/${folder}/info.json`);
             let response = await a.json();
             console.log(response)
-        }
-    })
+            cardContainer.innerHTML = cardContainer.innerHTML + ` <div data-folder="cs" class="card">
+            <!-- <div class="play"> -->
+            <div class="play">
+                <div class="play-container">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24"
+                        fill="#33333">
+                        <path
+                            d="M18.8906 12.846C18.5371 14.189 16.8667 15.138 13.5257 17.0361C10.296 18.8709 8.6812 19.7884 7.37983 19.4196C6.8418 19.2671 6.35159 18.9776 5.95624 18.5787C5 17.6139 5 15.7426 5 12C5 8.2574 5 6.3861 5.95624 5.42132C6.35159 5.02245 6.8418 4.73288 7.37983 4.58042C8.6812 4.21165 10.296 5.12907 13.5257 6.96393C16.8667 8.86197 18.5371 9.811 18.8906 11.154C19.0365 11.7084 19.0365 12.2916 18.8906 12.846Z"
+                            stroke="black" stroke-width="1.5" stroke-linejoin="round" />
+                    </svg>
+                </div>
+            </div>
 
+            <!-- </div> -->
+
+            <img src="songs/${folder}/cover.jpg" alt="">
+            <h2>${response.title}</h2>
+            <p>${response.description}</p>
+
+        </div>`
+        }
+    }
+
+
+    // load the playlist whenever card is clicked
+    Array.from(document.getElementsByClassName("card")).forEach(e => {
+        e.addEventListener("click", async item => {
+            // console.log(item, item.currentTarget.dataset)
+            songs = await getSongs(`songs/${item.currentTarget.dataset.folder}`);
+        })
+    })
 }
 
 
@@ -168,12 +201,6 @@ async function main() {
         currentSong.volume = parseInt(e.target.value) / 100;
     })
 
-    // load the playlist whenever card is clicked
-    Array.from(document.getElementsByClassName("card")).forEach(e => {
-        e.addEventListener("click", async item => {
-            // console.log(item, item.currentTarget.dataset)
-            songs = await getSongs(`songs/${item.currentTarget.dataset.folder}`);
-        })
-    })
+
 }
 main()
